a240ffd262cc54c3d3e7d58807b094ca5aba92ae
pete <pete.barber@gmail.com>
2015-06-06 10:41:35 +0100

    Initial commit. Nothing useful yet, just testing that Pod is configured correctly

---
+++ KeyChainItemCRUDKit/KeyChainItemCRUDKit/KeyChainItemCRUD.swift
@@ -0,0 +1,21 @@
+//
+//  first.swift
+//  KeyChainItemCRUDKit
+//
+//  Created by Pete on 06/06/2015.
+//  Copyright (c) 2015 KeyChainItemCRUDKit. All rights reserved.
+//
+
+import Foundation
+
+public class KeyChainItemCRUD
+{
+	public init()
+	{
+	}
+	
+	public func f(value: String) -> String
+	{
+		return "Hello, \(value)"
+	}
+}
---
+++ KeyChainItemCRUDKit/KeyChainItemCRUDKit/KeyChainItemCRUDKit.h
@@ -0,0 +1,19 @@
+//
+//  KeyChainItemCRUDKit.h
+//  KeyChainItemCRUDKit
+//
+//  Created by Pete on 06/06/2015.
+//  Copyright (c) 2015 KeyChainItemCRUDKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyChainItemCRUDKit.
+FOUNDATION_EXPORT double KeyChainItemCRUDKitVersionNumber;
+
+//! Project version string for KeyChainItemCRUDKit.
+FOUNDATION_EXPORT const unsigned char KeyChainItemCRUDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyChainItemCRUDKit/PublicHeader.h>
+
+
